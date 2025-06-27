4a1c5ea760d8763132a953ea2e6355645893278f
jaesparz <jaesparz@espol.edu.ec>
2025-06-26 22:44:58 -0500

    BlackJack FUNCIONAL con reglas propias

--- ProyectoBlackJack.asm
+++ ProyectoBlackJack.asm
@@ -1,6 +1,4 @@
 .data
-mazo:   .word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10  # Mazo de 22 cartas (2 copias de cada carta)
-
 mensaje_decision: .asciiz "\n¿Qué deseas hacer? (1 = Pedir carta, 0 = Plantarse): "
 mensaje_turno_jugador1: .asciiz "\n--- Turno del Jugador 1 (Tú) \n"
 mensaje_turno_jugador2: .asciiz "\n--- Turno del Jugador 2 (Máquina) \n"
@@ -18,181 +16,197 @@ mensaje_empate: .asciiz "\n¡Empate!\n"
 .globl main
 
 main:
-    # Inicialización
-    li $s2, 0                  # Puntaje del jugador 1 (Usuario)
-    li $s3, 0                  # Puntaje del jugador 2 (Máquina)
-    li $s4, 0                  # Índice de la carta actual en el mazo
-    li $s5, 0                  # Contador de turnos (máximo de turnos)
-    li $t0, 10                 # Límite de turnos (10 turnos en total)
+    # Inicialización de registros
+    li $s2, 0                  # Puntaje jugador 1
+    li $s3, 0                  # Puntaje jugador 2
+    li $s4, 0                  # Índice del mazo (no usado ahora)
 
-    # Repartir 2 cartas al jugador 1 y jugador 2
+    # Cartas iniciales
     jal repartir_cartas
 
 game_loop:
-    # Turno del jugador 1 (Usuario)
-    jal manejar_decision_jugador1
+    # Turno jugador humano
+    jal jugador1_turno
+    bgt $s2, 21, jugador1_pasado  # Si se pasa, pierde
 
-    # Verificar si el jugador 1 se pasó de 21
-    bgt $s2, 21, mensaje_perdedor
+    # Turno máquina
+    jal jugador2_turno
+    bgt $s3, 21, jugador2_pasado  # Si se pasa, ganas
 
-    # Turno del jugador 2 (Máquina)
-    jal manejar_decision_jugador2
-
-    # Verificar si el jugador 2 se pasó de 21
-    bgt $s3, 21, mensaje_ganador
-
-    # Incrementar contador de turnos
-    addi $s5, $s5, 1
-
-    # Si se alcanza el límite de turnos, comparar puntajes
-    bge $s5, $t0, comparar_puntajes
-
-    # Volver al inicio del bucle
+    # Continuar juego
     j game_loop
 
 repartir_cartas:
-    # Reparte 2 cartas a cada jugador
-    lw $t1, mazo($s4)
+    li $t5, 2                # 2 cartas por jugador
+
+repartir_j1:
+    li $v0, 42               # Random int [1-10]
+    li $a1, 10
+    syscall  
+    addi $t1, $a0, 1
     add $s2, $s2, $t1
-    addi $s4, $s4, 4
-
-    lw $t2, mazo($s4)
-    add $s3, $s3, $t2
-    addi $s4, $s4, 4
-
-    lw $t3, mazo($s4)
-    add $s2, $s2, $t3
-    addi $s4, $s4, 4
-
-    lw $t4, mazo($s4)
-    add $s3, $s3, $t4
-    addi $s4, $s4, 4
-
+    
+    addi $t5, $t5, -1
+    bnez $t5, repartir_j1
+    
+    li $t5, 2                # Reset contador
+    
+repartir_j2:
+    li $v0, 42               # Random int [1-10]
+    li $a1, 10
+    syscall  
+    addi $t1, $a0, 1
+    add $s3, $s3, $t1
+    
+    addi $t5, $t5, -1
+    bnez $t5, repartir_j2
+    
     jr $ra
 
-manejar_decision_jugador1:
-    # Mostrar turno y puntaje
+jugador1_turno:
+    # Mostrar info
     li $v0, 4
     la $a0, mensaje_turno_jugador1
     syscall
-
+    
     li $v0, 4
     la $a0, mensaje_puntaje
     syscall
-
+    
     li $v0, 1
     move $a0, $s2
     syscall
-
-    # Pedir decisión al usuario
+    
+    # Pedir decisión
     li $v0, 4
     la $a0, mensaje_decision
     syscall
-
+    
     li $v0, 5
     syscall
-    move $t1, $v0
+    
+    beq $v0, 1, robar_j1
+    j plantarse_j1
 
-    beq $t1, 1, pedir_carta1
-    beq $t1, 0, plantarse1
-
-pedir_carta1:
-    lw $t2, mazo($s4)
-    add $s2, $s2, $t2
-    addi $s4, $s4, 4
-
+robar_j1:
+    li $v0, 42               # Random card [1-10]
+    li $a1, 10
+    syscall  
+    addi $t1, $a0, 1
+    
+    add $s2, $s2, $t1
+    
     li $v0, 4
     la $a0, mensaje_acción_jugador_roba
     syscall
-
+    
     li $v0, 1
-    move $a0, $t2
+    move $a0, $t1
     syscall
-
+    
     jr $ra
 
-plantarse1:
+plantarse_j1:
     li $v0, 4
     la $a0, mensaje_acción_jugador_planta
     syscall
-
+    
+    # Si ambos plantados, comparar
+    bnez $s6, comparar_puntajes
+    
+    li $s6, 1                # Marcar que J1 se plantó
     jr $ra
 
-manejar_decision_jugador2:
-    # Mostrar turno y puntaje
+jugador2_turno:
+    # Mostrar info
     li $v0, 4
     la $a0, mensaje_turno_jugador2
     syscall
-
+    
     li $v0, 4
     la $a0, mensaje_puntaje_jugador2
     syscall
-
+    
     li $v0, 1
     move $a0, $s3
     syscall
-
-    # Lógica de decisión automática para la máquina
-    blt $s3, 15, pedir_carta2  # Si <15, siempre pide carta
-
-    # Si el puntaje está entre 15 y 18
-    li $v0, 42                 # Syscall para obtener un número aleatorio
-    li $a1, 100                # Rango de 0 a 99
+    
+    # Lógica IA
+    blt $s3, 15, robar_j2    # <15 siempre roba
+    
+    li $v0, 42               # 15-17: 70% robar
+    li $a1, 100
     syscall
+    li $t0, 70
+    blt $a0, $t0, robar_j2
+    
+    j plantarse_j2           # ?18 siempre planta
 
-    li $t0, 70                 # 70% de probabilidad de robar
-    blt $a0, $t0, pedir_carta2 # Si el número aleatorio es menor que 70, pide carta
-
-    # Si el puntaje es 18 o más, se planta
-    j plantarse2
-
-pedir_carta2:
-    lw $t3, mazo($s4)
-    add $s3, $s3, $t3
-    addi $s4, $s4, 4
-
+robar_j2:
+    li $v0, 42               # Random card [1-10]
+    li $a1, 10
+    syscall  
+    addi $t1, $a0, 1
+    
+    add $s3, $s3, $t1
+    
     li $v0, 4
     la $a0, mensaje_acción_jugador2_roba
     syscall
-
+    
     li $v0, 1
-    move $a0, $t3
+    move $a0, $t1
     syscall
-
+    
     jr $ra
 
-plantarse2:
+plantarse_j2:
     li $v0, 4
     la $a0, mensaje_acción_jugador2_planta
     syscall
-
+    
+    # Si ambos plantados, comparar
+    bnez $s6, comparar_puntajes
+    
+    li $s6, 1                # Marcar que J2 se plantó
     jr $ra
 
-comparar_puntajes:
-    bgt $s2, 21, mensaje_perdedor
-    bgt $s3, 21, mensaje_ganador
-
-    beq $s2, $s3, empate
-    bgt $s2, $s3, mensaje_ganador
-    blt $s2, $s3, mensaje_perdedor
-
-empate:
-    li $v0, 4
-    la $a0, mensaje_empate
-    syscall
-    j fin
-
-mensaje_perdedor:
+jugador1_pasado:
     li $v0, 4
     la $a0, mensaje_perdedor_juego
     syscall
     j fin
 
-mensaje_ganador:
+jugador2_pasado:
     li $v0, 4
     la $a0, mensaje_ganador_juego
     syscall
     j fin
+
+comparar_puntajes:
+    bgt $s2, 21, jugador1_pasado
+    bgt $s3, 21, jugador2_pasado
+    
+    beq $s2, $s3, empate
+    bgt $s2, $s3, jugador1_gana
+    j jugador2_gana
+
+jugador1_gana:
+    li $v0, 4
+    la $a0, mensaje_ganador_juego
+    syscall
+    j fin
+
+jugador2_gana:
+    li $v0, 4
+    la $a0, mensaje_perdedor_juego
+    syscall
+    j fin
+
+empate:
+    li $v0, 4
+    la $a0, mensaje_empate
+    syscall
 
 fin:
     li $v0, 10
